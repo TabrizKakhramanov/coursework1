b47f400d1d3e0dca0107798d05803c0204d4a7f7
TabrizKakhramanov <tabriz9998@gmil.com>
2024-11-01 17:44:59 +0300

    Made homework1

---
+++ src/Employee.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Employee {
+    static int id;
+    private String name;
+    private int salary;
+    private int department;
+
+    public Employee(String name, int salary, int department) {
+        this.name = name;
+        this.salary = salary;
+        this.department= department;
+        id++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return salary == employee.salary && Objects.equals(name, employee.name) && Objects.deepEquals(department, employee.department);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, salary, department);
+    }
+    @Override
+    public String toString() {
+        return "ФИО сотрудника: "+this.name+", отдел: "+department+ ", зарплата: "+salary+".";
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
 public class Main {
     public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+        employees[0]= new Employee("Иван Иванович Иванов0", 30_000, 1);
+        employees[1]= new Employee("Иван Иванович Иванов1", 40_000, 2);
+        employees[2]= new Employee("Иван Иванович Иванов2", 55_000, 3);
+        employees[3]= new Employee("Иван Иванович Иванов3", 60_000, 3);
+        employees[4]= new Employee("Иван Иванович Иванов4", 70_000, 4);
+        employees[5]= new Employee("Иван Иванович Иванов5", 80_000, 4);
+        employees[6]= new Employee("Иван Иванович Иванов6", 90_000, 4);
+        employees[7]= new Employee("Иван Иванович Иванов7", 20_000, 1);
+        employees[8]= new Employee("Иван Иванович Иванов8", 150_000, 5);
+        employees[9]= new Employee("Иван Иванович Иванов9", 60_000, 2);
+
+
+
+
+
+
+        printAllEmployees(employees);
+        System.out.println("Общая зарплата: "+summarySalary(employees));
+        System.out.println("Минимальная зарплата: "+minSalary(employees));
+        System.out.println("Максимальная зарплата: "+maxSalary(employees));
+        System.out.println("Средняя зарплата: "+averageSalary(employees));
+        printAllNames(employees);
+
 
     }
+    public static void printAllEmployees(Employee[] employees){
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].toString());
+        }
+    }
+    public static int summarySalary(Employee[] employees){
+        int sum=0;
+        for (int i = 0; i < employees.length; i++) {
+            sum+= employees[i].getSalary();
+        }
+        return sum;
+    }
+
+    public static int minSalary(Employee[] employees){
+        int min= Integer.MAX_VALUE;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary()<min){
+                min = employees[i].getSalary();
+            }
+        }
+        return min;
+    }
+    public static int maxSalary(Employee[] employees){
+        int max= Integer.MIN_VALUE;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary()>max){
+                max = employees[i].getSalary();
+            }
+        }
+        return max;
+    }
+    public static int averageSalary(Employee[] employees){
+        int average = summarySalary(employees)/employees.length;
+        return average;
+    }
+    public static void printAllNames(Employee[] employees){
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].getName());
+        }
+    }
+
 }
